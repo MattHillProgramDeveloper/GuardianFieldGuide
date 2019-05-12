c4857928acf47df750f9a0e2d1a5f4f0240832b8
Matt Hill <mlhill1@fullsail.edu>
2019-05-12 12:49:51 -0400

    First Try With Getting Started xhr

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var apiKey = "295d9b8a41bf489a9987d980ee8260a4";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
+xhr.setRequestHeader("X-API-Key", apiKey);
+
+xhr.onreadystatechange = function(){
+ if(this.readyState === 4 && this.status === 200){
+  var json = JSON.parse(this.responseText);
+  console.log(json.Response.data.inventoryItem.itemName); //Gjallarhorn
+ }
+}
+
+xhr.send();
